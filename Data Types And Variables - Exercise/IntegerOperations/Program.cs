2cde31d7358ef1f26b5f884a7fcc29ac69a9ddeb
Nikola Nikolov <90271515+nkla-Nikolov@users.noreply.github.com>
2021-12-17 21:13:54 +0200

    initial commit

---
+++ Data Types And Variables - Exercise/IntegerOperations/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace IntegerOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int num4 = int.Parse(Console.ReadLine());
+
+            int result = (AddFirstTwo(num1, num2) / num3) * num4;
+
+            Console.WriteLine(result);
+        }
+
+        static int AddFirstTwo(int a, int b)
+        {
+            int result = a + b;
+            return result;
+        }
+    }
+}
---
+++ Data Types And Variables - Exercise/Pokemon/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Collections;
+
+
+namespace Pokemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            double originalValueOfN = n * 0.5;
+            int targets = 0;
+            
+            while (n >= m)
+            {
+                n -= m;
+                if (n == originalValueOfN)
+                {
+                    n /= y;
+                    
+                }
+                
+                targets++;
+            }
+
+            Console.WriteLine(n);
+            Console.WriteLine(targets);
+        }
+    }
+}
